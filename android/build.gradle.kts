66c08c33fe8d0cd1d2b84b338b98eb18383842ac
joe-weston <josephmweston@gmail.com>
2026-05-20 20:19:27 -0400

    folder re-organize

--- android/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-// Top-level build file. Plugin versions live in the version catalog (gradle/libs.versions.toml).
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-}
